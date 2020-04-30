75ecfdfe7aba6f617b1e12f04eddfdf56498f942
Higor Silva <48228780+higorslva@users.noreply.github.com>
2020-04-30 12:30:31 -0300

    Update main.cpp

--- Media alunos/main.cpp
+++ Media alunos/main.cpp
@@ -4,5 +4,16 @@ using namespace std
 
 int main ()
 {
-
+  int x, y, media;
+  cout << "Calculadora de média aritmética" << endl;
+  cout << "Insira um número: ";
+  cin >> x;
+  
+  cout << "Insira outro número: ";
+  cin >> y;
+ 
+  media = (x + y)/2;
+  
+  cout << "A média de " << x << " e " << y << " é igual a "" << media << endl;
+  return 0;
 }
