d886378b1d3b462d1d14d9342a34043adaf49345
Higor Silva <higors88@gmail.com>
2020-09-30 22:51:54 -0300

    aleatorios

---
+++ C++ aleatorios/C++/teste_for.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main ()
+{
+    float i, x;
+    for(x = 100);
+         i = 0; i <= 100; i++);
+    {
+        std::cout << "x" << ", ";
+    }
+    
+}
---
+++ C++ aleatorios/teste.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+void Teste ()
+{
+    cout << "Teste" << endl;
+}
+
+int main ()
+{
+    Teste;
+    return 0;
+}
